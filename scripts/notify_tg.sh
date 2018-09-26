6993fd220c11e6471df459e5e80d85743065d5ad
vvy <vemelin@ooyala.com>
2018-09-26 20:44:06 +0300

    update notification format

--- scripts/notify_tg.sh
+++ scripts/notify_tg.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+NEWLINE=$'\n'
 curl -X POST https://api.telegram.org/bot657121652:AAEz_Uy77hcSBx5Brd_DA3F62J6yKmZBCmU/sendMessage -d chat_id=-1001361347769 \
--d "text=${TRAVIS_BRANCH} (${TRAVIS_COMMIT_MESSAGE}) tests return code: ${TRAVIS_TEST_RESULT}"
+-d "text=branch:${TRAVIS_BRANCH}${NEWLINE}commit:${TRAVIS_COMMIT}${NEWLINE}${TRAVIS_COMMIT_MESSAGE}${NEWLINE}tests return code: ${TRAVIS_TEST_RESULT}"
